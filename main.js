3e74151b497c845e0a312d0a3ec867340a5db5f9
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-07-09 20:34:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,6 @@
         'imgur',
         'strToImg'
     ].map(v => `https://rpgen3.github.io/mylib/export/${v}.mjs`));
-    const undef = void 0;
     const html = $('body').css({
         'text-align': 'center',
         padding: '1em',
@@ -15,81 +14,131 @@
     const header = $('<div>').appendTo(html),
           body = $('<div>').appendTo(html),
           footer = $('<div>').appendTo(html);
-    class Mover {
+    const sysImg = await new Promise.all([
+        'aY2ef1p', // 404
+        'oypcplE' // now loading
+    ].map(id => rpgen3.imgur.load(id)));
+    const unit = 16;
+    let zoom = 3;
+    class Sprite {
         constructor(id){
-            this.x = this.y = 0;
-            this.z = 0;
+            this.img = sysImg[1];
             rpgen3.imgur.load(id).then(img => {
                 this.img = img;
-                if(this.w !== undef && this.h !== undef) return;
-                this.w = img.width;
-                this.h = img.height;
+                const {width, height} = img;
+                if(width === unit * 2) this.draw = new Anime(img).draw;
+            }).catch(() => {
+                this.img = sysImg[0];
             });
         }
-        set z(v){
-            if(this.delete) this.delete();
-            this.delete = layer.set(this, v);
+        draw(ctx, x, y){
+            ctx.drawImage(
+                this.img,
+                x, y, Sprite, Sprite
+            );
         }
-        update(ctx){
-            if(this.img) ctx.drawImage(this.img, this.x, this.y);
-        }
-        goto(x, y){
-            this.x = x;
-            this.y = y;
-            return this;
-        }
-        move(x, y){
-            this.x += x;
-            this.y += y;
+        static valueOf(){
+            return unit * zoom;
         }
     }
-    class Anime extends Mover {
-        constructor(...arg){
-            super(...arg);
-            const rate = 3;
-            this.w = this.h = 16 * rate;
+    class Anime {
+        constructor(img){
+            this.img = img;
             this.anime = 500;
             this.direct = 'd';
         }
-        update(ctx){
-            if(!this.img || this.hide) return;
-            const {img, x, y, w, anime, direct} = this,
+        draw(ctx, x, y){
+            const {img, anime, direct} = this,
                   index = 'wdsa'.indexOf(direct),
-                  xx = g_nowTime % anime < anime / 2 ? 0 : 1,
-                  unit = 16;
+                  xx = g_nowTime % anime < anime / 2 ? 0 : 1;
             ctx.drawImage(
                 img,
                 xx * unit, index * unit, unit, unit,
-                x, y, w, w
+                x, y, Sprite, Sprite
             );
         }
-        move(x, y){
-            super.move(x, y);
-            if(x < 0) this.direct = 'a';
-            else if(x > 0) this.direct = 'd';
-            else if(y < 0) this.direct = 'w';
-            else if(y > 0) this.direct = 's';
-        }
     }
-    class Player extends Anime {
+    const imgurMap = new class {
+        constructor(){
+            this.m = new Map;
+        }
+        set(id){
+            if(!this.m.has(id)) this.m.set(id, new Sprite(id));
+        }
+        get(id){
+            return this.m.get(id);
+        }
+    };
+    const frame = new class {
+        constructor(){
+            this.x = this.y = 0;
+        }
+        set(w, h){
+            this.w = w;
+            this.h = h;
+        }
+        goto(x, y){
+            const {w, h} = this,
+                  {width, height} = g_dqMap.info,
+                  ww = width - w,
+                  hh = height - h;
+            this.x = x < 0 || ww < 0 ? 0 : x > ww ? ww : x;
+            this.y = y < 0 || hh < 0 ? 0 : y > hh ? hh : y;
+            return [this.x === x, this.y === y];
+        }
+    };
+    const player = new class {
+        constructor(){
+            this.x = this.y = 0;
+        }
+        dressUp(id){
+            this.id = id;
+            imgurMap.set(id);
+        }
         update(ctx){
-            if(!this.img) return;
-            if(isKeyDown(['ArrowUp','z','w',' ',undef])) this.jump();
-            if(isKeyDown(['ArrowLeft','a'])) this.move(-5,0);
-            if(this.x < 0) {
-                this.x = 0;
-                this.wall();
-            }
-            if(isKeyDown(['ArrowRight','d'])) this.move(5,0);
-            if(this.x + this.w > cv.w) {
-                this.x = cv.w - this.w;
-                this.wall();
-            }
-            this.hide = this._damage && g_nowTime % 200 < 100;
-            super.update(ctx);
-            this.updateQ();
+            if(isKeyDown(['ArrowLeft','a'])) this.move(1, 0);
+            else if(isKeyDown(['ArrowRight','d'])) this.move(-1, 0);
+            else if(isKeyDown(['ArrowUp','w'])) this.move(0, -1);
+            else if(isKeyDown(['ArrowDown','s'])) this.move(0, 1);
         }
-    }
+        goto(x, y){
+            const [isX, isY] = frame.goto(x, y);
+            const {w, h} = frame,
+                  {width, height} = g_dqMap.info,
+                  ww = width - w,
+                  hh = height - h;
+        }
+        move(x, y){
+            this.goto(this.x + x, this.y + y);
+        }
+    };
+    const rpgen4 = await importAllSettled([
+        'isKeyDown',
+        'Canvas',
+        'DQMap',
+        'layer'
+    ].map(v => `https://rpgen3.github.io/game/export/${v}.mjs`));
+    const {isKeyDown, layer} = rpgen4,
+          cv = new rpgen4.Canvas(footer).set(0.9, 0.7),
+          g_dqMap = new rpgen4.DQMap();
+    layer.set({
+        update: ctx => {
+            if(!g_dqMap.data) return;
+            const {info, define, data} = g_dqMap,
+                  {width, height, depth} = info,
+                  {x, y, w, h} = frame;
+            for(let i = 0; i < depth; i++) for(let j = 0; j < h; j++) for(let k = 0; k < w; k++) imgurMap.get(define[data[i][j + y]?.[k + x]])?.draw(ctx, x, y);
+        }
+    });
+    let g_nowTime;
+    const update = () => {
+        g_nowTime = performance.now();
+        const {ctx, w, h} = cv;
+        ctx.clearRect(0, 0, w, h);
+        layer.forEach(v => v.update(ctx));
+        requestAnimationFrame(update);
+    };
+    update();
     class SimpleText {
         constructor({text = '', color = 'black', size = 16}){
             this.x = this.y = 0;
@@ -112,28 +161,6 @@
             return this;
         }
     }
-    const rpgen4 = await importAllSettled([
-        'isKeyDown',
-        'Canvas',
-        'layer'
-    ].map(v => `https://rpgen3.github.io/game/export/${v}.mjs`));
-    const {isKeyDown, layer} = rpgen4,
-          cv = new rpgen4.Canvas(footer).set(0.9, 0.7),
-          g_horizonY = {
-              valueOf: () => cv.h * 0.9
-          };
-    const mass = [...new Array()]; // 3次元配列
-    let g_nowTime;
-    const update = () => {
-        g_nowTime = performance.now();
-        const {ctx} = cv;
-        ctx.clearRect(0, 0, cv.w, cv.h);
-        const {x,y,width,height,depth} = now;
-        for(let i = 0; i < height; i++) for(let j = 0; j < width; j++) for(let k = 0; k < depth; k++) mass[i + y][j + x][k].draw?.(ctx, i, j);
-        layer.forEach(v => v.update(ctx));
-        requestAnimationFrame(update);
-    };
-    update();
     new SimpleText({
         text: {
             toString: () => `座標(${x},${y})`
