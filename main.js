fea218fe714f22b7db51b827b99467e39be0f7b0
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-07-10 00:03:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 (async()=>{
+    let g_debug;
     const {importAll, importAllSettled, getScript} = await import('https://rpgen3.github.io/mylib/export/import.mjs');
     await getScript('https://rpgen3.github.io/lib/lib/jquery-3.5.1.min.js');
     const rpgen3 = await importAll([
@@ -6,7 +7,7 @@
         'imgur',
         'strToImg'
     ].map(v => `https://rpgen3.github.io/mylib/export/${v}.mjs`));
-    const html = $('body').css({
+    const html = $('body').empty().css({
         'text-align': 'center',
         padding: '1em',
         'user-select': 'none'
@@ -14,7 +15,7 @@
     const header = $('<div>').appendTo(html),
           body = $('<div>').appendTo(html),
           footer = $('<div>').appendTo(html);
-    const sysImg = await new Promise.all([
+    const sysImg = await Promise.all([
         'aY2ef1p', // 404
         'oypcplE' // now loading
     ].map(id => rpgen3.imgur.load(id)));
@@ -98,25 +99,30 @@
     };
     const player = new class {
         constructor(){
-            this.x = this.y = this._x = this._y = this.subX = this.subY = this.nowX = this.nowY = this._move = 0;
-            this.speed = 0.1;
+            this.x = this.y = this._x = this._y = this.subX = this.subY = this.nowX = this.nowY = 0;
+            this.time = 300;
+            this._time = null;
         }
         dressUp(id){
             this.id = id;
             imgurMap.set(id);
         }
         update(ctx){
-            if(isKeyDown(['ArrowLeft','a'])) this.move(1, 0);
-            else if(isKeyDown(['ArrowRight','d'])) this.move(-1, 0);
+            if(isKeyDown(['ArrowLeft','a'])) this.move(-1, 0);
+            else if(isKeyDown(['ArrowRight','d'])) this.move(1, 0);
             else if(isKeyDown(['ArrowUp','w'])) this.move(0, -1);
             else if(isKeyDown(['ArrowDown','s'])) this.move(0, 1);
-            const {x, y, _x, _y, _move, speed} = this;
-            if(_move) {
-                _move -= speed;
-                if(_move < 0) _move = 0;
+            const {x, y, _x, _y, time, _time} = this;
+            let rate = 1;
+            if(this._time){
+                rate = 1 - (g_nowTime - _time) / this.time;
+                if(rate <= 0) {
+                    rate = 0;
+                    this._time = null;
+                }
             }
-            this.subX = (x - _x) * _move;
-            this.subY = (y - _y) * _move;
+            this.subX = (x - _x) * rate;
+            this.subY = (y - _y) * rate;
             this.nowX = x - this.subX;
             this.nowY = y - this.subY;
             //imgurMap.get(this.id).draw(ctx, this.nowX, this.nowY); 表示しなければセーフ
@@ -129,19 +135,20 @@
             this.y = y < 0 ? 0 : y >= height ? height - 1 : y;
         }
         move(x, y){
+            if(this._time) return;
+            this._time = g_nowTime;
             this._x = this.x;
             this._y = this.y;
-            this._move = 1;
             this.goto(this.x + x, this.y + y);
         }
     };
     const z = 0;
     const now = '1XPXQAe';
     const setSprite = (x, y) => {
-        g_dqMap[z][y][x] = now;
+        g_dqMap.data[z][y][x] = now;
     };
     const deleteSprite = (x, y) => {
-        g_dqMap[z][y][x] = null;
+        g_dqMap.data[z][y][x] = null;
     };
     const rpgen4 = await importAllSettled([
         'isKeyDown',
@@ -151,7 +158,9 @@
     ].map(v => `https://rpgen3.github.io/game/export/${v}.mjs`));
     const {isKeyDown, layer} = rpgen4,
           cv = new rpgen4.Canvas(footer).set(0.9, 0.7),
-          g_dqMap = new rpgen4.DQMap().set(15, 11).init();
+          g_dqMap = new rpgen4.DQMap().set(15, 11, 3).init();
+    g_dqMap.define = {[now]:now};
+    window.g_dqMap = g_dqMap;
     layer.set(frame);
     layer.set(player);
     let g_nowTime;
@@ -192,4 +201,11 @@
         size: 30,
         color: 'blue'
     });
+    new SimpleText({
+        text: {
+            toString: () => `debug=${g_debug}`
+        },
+        size: 30,
+        color: 'blue'
+    }).goto(0, 30);
 })();
