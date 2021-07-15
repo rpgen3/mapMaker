65aba4db23205f6fcce79ac32173bc5870f4ba26
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-07-15 23:44:54 +0900

    Update DQMap.mjs

--- mjs/DQMap.mjs
+++ mjs/DQMap.mjs
@@ -1,9 +1,7 @@
-import {BaseN} from 'https://rpgen3.github.io/mylib/export/BaseN.mjs';
-const base62 = new BaseN('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz');
-export class DQMap {
+class DQMap {
     constructor(){
         this.info = {};
-        this.define = new Define;
+        this.define = new Map;
     }
     set(width, height, depth){
         const {info} = this;
@@ -19,74 +17,70 @@ export class DQMap {
     }
     make(){
         const {height, width} = this.info;
-        return [...new Array(height)].map(() => [...new Array(width).fill(-1)]);
+        return [...new Array(height)].map(() => [...new Array(width).fill(null)]);
     }
     input(str){ // 文字列からマップデータを読み込む
-        const ar = ['info', 'define', 'data'].map(v => str.match(new RegExp(`#${v}[^#]+`, 'g'))[0]),
-              keys = ['width', 'height', 'depth'];
-        this.info = toObj(ar[0], Number);
-        const define = new Define;
-        for(const [k,v] of Object.entries(toObj(ar[1]))) define.set(base62.decode(k), v);
-        this.define = define;
-        this.data = parse(ar[2], define._a);
+        const [info, define, data] = ['info', 'define', 'data'].map(v => str.match(new RegExp(`#${v}[^#]+`, 'g'))?.[0]);
+        if([info, define, data].some(v => !v)) throw new Error('DQMap needs #info, #define and #data');
+        this.info = toArr(info).reduce((p, [k, v]) => (p[k] = Number(v), p), {});
+        this.define = toMap(toArr(define));
+        this.data = parse(data, this.define);
         return this;
     }
-    output(char = '', zArr = [...new Array(this.depth).keys()]){ // マップデータを文字列化
+    output(zArr = [...new Array(this.depth).keys()]){ // マップデータを文字列化
         const m = new Map,
               {info, define, data} = this,
               ar = [
                   Object.entries(info),
-                  [...define._c].map(([k, v])=>[define._b.get(k), v])
-              ].map(v => v.map(v => v.join(':')).join('\n'));
+                  toStr(define)
+              ].map(v => v.map(v => v.join(': ')).join('\n'));
         m.set('info', ar[0]);
         m.set('define', ar[1]);
-        m.set('data', stringify({...info, data, char, zArr, _b: define._b}));
+        m.set('data', stringify({...info, define, data, zArr}));
         return [...m].map(([k,v]) => `#${k}\n${v}`).join('\n\n');
     }
 }
-class Define {
-    constructor(){
-        this._a = new Map; // str to int
-        this._b = new Map; // int to str
-        this._c = new Map; // int to id
-    }
-    set(int, id){
-        this._c.set(int, id);
-        const str = base62.encode(int);
-        this._a.set(str, int);
-        this._b.set(int, str);
-    }
-    get(int){
-        return this._c.get(int);
-    }
-    delete(int){
-        this._c.delete(int);
-        const str = this._b.get(int);
-        this._a.delete(str);
-        this._b.delete(int);
-    }
-    get keys(){
-        return this._c.keys();
-    }
-    get next(){
-        return Math.max(...this.keys, -1) + 1;
-    }
-};
-const nameRule = s => s.match(/[0-9A-Za-z]+/);
-const toObj = (str, func = v => v) => {
-    const obj = {};
+const toArr = str => {
+    const a = [];
     for(const line of str.split('\n')){
-        const ar = [];
-        for(const v of line.split(':')){
-            const m = nameRule(v);
-            if(m) ar.push(m[0]);
-            else break;
-        }
-        if(ar.length === 2) obj[ar[0]] = func(ar[1]);
+        const m = line.match(/^[0-9A-Za-z]+:/)?.[0];
+        if(m) a.push([m.slice(0, -1), line.slice(m.length)]);
     }
-    return obj;
+    return a;
 };
-const parse = (data, _a) => {
+const toMap = arr => {
+    const map = new Map;
+    for(const [k, v] of arr){
+        if(/[^0-9]/.test(k)) continue;
+        const key = Number(k);
+        if(map.has(key)) throw new Error(`#define has same keys of ${key}`);
+        const [v0, v1] = v.split('['),
+              index = [];
+        if(v1) for(const v of v1.split(',')) {
+            const n = toInt(v);
+            if(!Number.isNaN(n)) index.push(n);
+        }
+        const arg = v0.split(',');
+        let i = 0;
+        const next = () => arg[i++],
+              obj = {};
+        obj.id = next().match(/[0-9A-Za-z]+/)?.[0];
+        if(/[wasd]/.test(arg[2])) {
+            obj.flame = toInt(next());
+            obj.direct = next().replace(/[^wasd]/g, '')
+        }
+        if(index.length) {
+            obj.width = toInt(next());
+            const height = toInt(next());
+            if(!Number.isNaN(height)) obj.height = height;
+            obj.index = index;
+        }
+        map.set(key, obj);
+    }
+    return map;
+};
+const toInt = str => Number(str?.match(/[0-9]+/)?.[0]);
+const parse = (data, define) => {
     const arZ = [];
     for(const v of data.split(/$[^$]+/g)){
         const arY = [];
@@ -94,8 +88,19 @@ const parse = (data, _a) => {
             if(!line.includes(',')) continue;
             const arX = [];
             for(const e of line.split(',')){
-                const v = nameRule(e)?.[0];
-                arX.push(_a.has(v) ? _a.get(v) : -1);
+                const m = e.match(/[0-9]+/g)?.map(toInt),
+                      key = m?.[0];
+                if(define.has(key)) {
+                    const elm = {key},
+                          obj = define[key];
+                    if('direct' in obj) {
+                        const direct = e.match(/[wasd]/)?.[0];
+                        if(direct) elm.direct = direct;
+                    }
+                    if('index' in obj) elm.index = m[1];
+                    arX.push(elm);
+                }
+                else arX.push(null);
             }
             arY.push(arX);
         }
@@ -103,19 +108,51 @@ const parse = (data, _a) => {
     }
     return arZ;
 };
-const stringify = ({width, height, depth, data, char, zArr, _b}) => {
+const toStr = map => {
+    const a = [];
+    for(const [k,v] of map){
+        const ar = [],
+              {flame, direct, width, height, index} = v;
+        if('direct' in v){
+            ar.push(flame);
+            ar.push(direct);
+        }
+        if('index' in v && index.length){
+            ar.push(width);
+            if(height) ar.push(height);
+            ar.push('[' + index.join(', ') + ']');
+        }
+        a.push([k, ar.join(', ')]);
+    }
+};
+const stringify = ({width, height, depth, define, data, zArr}) => {
     const arZ = [];
     for(const z of zArr){
         const arY = [];
         for(let y = 0; y < height; y++){
             const arX = [];
             for(let x = 0; x < width; x++){
-                const v = data[z][y][x];
-                arX.push(_b.has(v) ? _b.get(v) : char);
+                const elm = data[z][y][x];
+                const v = (()=>{
+                    if(!elm) return;
+                    const {key, direct, index} = elm,
+                          obj = define[key];
+                    if(!obj) return;
+                    let str = key;
+                    if('index' in obj) {
+                        if(!obj.index.includes(index)) return;
+                        str += '-' + index;
+                    }
+                    if('direct' in obj) str += direct;
+                    return str;
+                })();
+                arX.push(v ? v : '');
             }
-            arY.push(arX.join(','));
+            arY.push(arX);
         }
-        arZ.push(`$${z}\n${arY.join('\n')}`);
+        arZ.push(arY);
     }
-    return arZ.join('\n\n');
+    let max = 0;
+    for(const z of arZ) for(const y of z) for(const x of y) if(max < x.length) max = x.length;
+    return arZ.map((z, i) => `$${i}\n` + z.map(y => y.map(x => x + ' '.repeat(max - x.length)).join(',')).join('\n')).join('\n\n');
 };
